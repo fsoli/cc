0df3ed2147a2c5c7159b6f7b0c48e21968f7041e
fsoli <78670344+fsoli@users.noreply.github.com>
2021-02-07 17:56:17 +0330

    Delete main_class.py

--- main_class.py
+++
@@ -1,9 +0,0 @@
-import collector_actor
-
-
-x=[]
-y=[]
-number_of_pcs = input('number_of_pcs: ')
-number_of_cities = input('number_of_cities: ')
-starter_actor = collector_actor.collector_actor.start(x, y,number_of_pcs,number_of_cities)
-starter_actor.ask({'message': 'init', 'starter_add': starter_actor})
